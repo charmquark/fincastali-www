5560466d3eff5e897933c671ef0824abfabd5ebe
Grant Nicholson-Sauls <ibisbasenji@gmail.com>
2015-01-02 08:06:30 -0600

    Initial integration of tumblr

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,5 +3,8 @@ class NewsController < ApplicationController
         @active_site_nav    = :news
         @body_id            = 'site-page-news'
         @title             << 'News'
+
+        client = Tumblr::Client.new
+        @posts = client.posts('fincastali-hearth.tumblr.com', limit: 5)['posts']
     end
 end
--- config/initializers/tumblr.rb
+++ config/initializers/tumblr.rb
@@ -1,5 +1,9 @@
 
 Tumblr.configure do |config|
-    
+    secrets = Rails.application.secrets.tumblr
+    config.consumer_key         = secrets['consumer_key']
+    config.consumer_secret      = secrets['consumer_secret']
+    config.oauth_token          = secrets['oauth_token']
+    config.oauth_token_secret   = secrets['oauth_token_secret']
 end
 
