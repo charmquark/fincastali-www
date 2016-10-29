c27a9d3951e1175b1a5cb2d7c943aef8772c6949
charmquark <ibisbasenji@gmail.com>
2016-10-28 21:23:47 -0500

    add slim-rails gem and adapt views to use Slim templates

---
+++ config/initializers/fincastali_www/cdn_urls.rb
@@ -0,0 +1,9 @@
+module FincastaliWww
+  #
+  module CdnUrls
+    CLOUDFLARE_BASE = 'https://cdnjs.cloudflare.com/ajax/libs/'.freeze
+
+    NORMALIZE = CLOUDFLARE_BASE + 'normalize/4.2.0/normalize.min.css'
+    MODERNIZR = CLOUDFLARE_BASE + 'modernizr/2.8.3/modernizr.min.js'
+  end
+end
