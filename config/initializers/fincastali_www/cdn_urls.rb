749223df3af2b7e8b8422799f373425c47b413e8
charmquark <ibisbasenji@gmail.com>
2016-12-21 05:09:09 -0600

    home page and fonts

--- config/initializers/fincastali_www/cdn_urls.rb
+++ config/initializers/fincastali_www/cdn_urls.rb
@@ -2,7 +2,10 @@ module FincastaliWww
   #
   module CdnUrls
     CLOUDFLARE_BASE = 'https://cdnjs.cloudflare.com/ajax/libs/'.freeze
+    FONTS_BASE      = 'https://fonts.googleapis.com/css?'.freeze
 
+    FONTS_CSS = FONTS_BASE +
+                'family=Caveat:400,700|Roboto:400,400i,700&amp;subset=latin-ext'
     NORMALIZE = CLOUDFLARE_BASE + 'normalize/4.2.0/normalize.min.css'
     MODERNIZR = CLOUDFLARE_BASE + 'modernizr/2.8.3/modernizr.min.js'
   end
